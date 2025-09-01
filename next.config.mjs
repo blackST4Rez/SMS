588d603957856ca5bf139b89d844805f6b3058d6
blackST4Rez <maharjanraka2015@gmail.com>
2025-09-01 13:26:52 +0545

    next.config.mjs file is modified

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  compiler: {
+    // Remove this if you want to use Babel instead of SWC
+    // But since we need Babel for Jest, we'll keep both
+  },
+  experimental: {
+    forceSwcTransforms: false,
+  }
+};
 
 export default nextConfig;
