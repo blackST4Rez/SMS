aa398a8daac93e4a0f0e4b2e66795f3a19870e35
blackST4Rez <maharjanraka2015@gmail.com>
2025-09-01 13:13:50 +0545

    RemoveBtn.jsx file is added

---
+++ components/RemoveBtn.jsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { HiOutlineTrash } from "react-icons/hi";
+import { useRouter } from "next/navigation";
+export default function RemoveBtn({id}){
+    const router = useRouter();
+    const removeTopic = async() => {
+        const confirmed = confirm('Are you sure you want to delete this student databse ?');
+
+        if (confirmed){
+            const res = await fetch(`/api/topics?id=${id}`,{
+                method:"DELETE",
+            });
+            if (res.ok){
+            router.refresh();
+            }
+        }
+    };
+    return (
+    <button onClick={removeTopic} className="text-red-400 cursor-pointer " >
+        <HiOutlineTrash size={24}/>
+    </button>
+    );
+}
